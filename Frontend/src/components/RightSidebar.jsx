8a3768a6338c338d0233dc6038f1db2e5df8374b
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2024-10-11 21:57:05 -0400

    Layout setup, tailwind and Mantine installed

---
+++ Frontend/src/components/RightSidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const RightSidebar = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-semibold">Right Sidebar</h2>
+      <p>Temp text</p>
+    </div>
+  );
+};
+
+export default RightSidebar;
